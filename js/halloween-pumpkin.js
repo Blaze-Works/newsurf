811a87873ccbf946656134bf8334473dc0475ac0
Blaze-Works <abodunrinsharon@gmail.com>
2025-11-03 11:19:44 +0100

    REBASING

--- js/halloween-pumpkin.js
+++ js/halloween-pumpkin.js
@@ -24,7 +24,11 @@ document.addEventListener('DOMContentLoaded', () => {
             position: fixed;
             width: 100px;
             height: 100px;
+<<<<<<< HEAD
             background-image: url('../assets/icon.pumpkin.png');
+=======
+            background-image: url('./assets/icon.pumpkin.png');
+>>>>>>> 98d74ca (edited  eeaster egg)
             background-size: contain;
             background-repeat: no-repeat;
             background-position: center;
